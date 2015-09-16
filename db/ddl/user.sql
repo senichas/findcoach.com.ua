c59863ca0b25b4aa57fd48d25c6d7d13e9ee638b
Aleksandr Krasilnikov <akrasilnikovwork@gmail.com>
2015-09-16 21:37:43 +0300

    Create user ddl

---
+++ db/ddl/user.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE "user"
+(
+  user_id serial NOT NULL,
+  first_name character varying(255) NOT NULL,
+  last_name character varying(255) NOT NULL,
+  email character varying(255) NOT NULL,
+  is_padawan boolean NOT NULL DEFAULT false,
+  is_coach boolean NOT NULL DEFAULT false,
+  is_active boolean NOT NULL DEFAULT false,
+  CONSTRAINT user_id_pk PRIMARY KEY (user_id),
+  CONSTRAINT user_email_uc UNIQUE (email)
+)
