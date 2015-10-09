d1fe31c25a15a493c2af02b8734adc8113faecb4
Andriy Senko <andriy_senko@epam.com>
2015-10-09 17:29:33 +0300

    Rename entity and add fields

---
+++ server-app/src/main/java/ua/com/findcoach/domain/Appointment.java
@@ -0,0 +1,14 @@
+package ua.com.findcoach.domain;
+
+/**
+ * Created by senich on 10/9/2015.
+ */
+public class Appointment {
+    private Long appointmentId;
+
+    private String title;
+
+    private String summary;
+
+
+}
