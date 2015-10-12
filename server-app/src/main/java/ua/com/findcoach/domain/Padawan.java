7a3ed6642e4253f0a2bfcc8fe44bc1c7fa8721ee
Aleksandr Krasilnikov <akrasilnikovwork@gmail.com>
2015-10-12 10:52:00 +0300

    Add Status.java and update Padawan.java

--- server-app/src/main/java/ua/com/findcoach/domain/Padawan.java
+++ server-app/src/main/java/ua/com/findcoach/domain/Padawan.java
@@ -1,8 +1,10 @@
 package ua.com.findcoach.domain;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("10")
 public class Padawan extends User {
 
 }
