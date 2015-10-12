3e9db30faa5319c1fd65724e35e182bf9478b0bd
Aleksandr Krasilnikov <akrasilnikovwork@gmail.com>
2015-10-12 11:53:55 +0300

    Add Status.java and update Padawan.java

--- server-app/src/main/java/ua/com/findcoach/domain/Padawan.java
+++ server-app/src/main/java/ua/com/findcoach/domain/Padawan.java
@@ -1,10 +1,29 @@
 package ua.com.findcoach.domain;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
-@DiscriminatorValue("10")
-public class Padawan extends User {
+@Table(name = "padawan")
+public class Padawan {
+    @OneToOne (cascade = CascadeType.ALL)
+    @JoinColumn (name = "user_id")
+    private User userId;
+    @Column (name = "user_status", nullable = false)
+    private PadawanStatus status;
 
+    public Padawan (){
+    }
+
+    @Enumerated(EnumType.STRING)
+    public PadawanStatus getStatus() {
+        return status;
+    }
+
+    public User getId(){
+        return userId;
+    }
+
+    public void setId (User userId){
+        this.userId=userId;
+    }
 }
--- server-app/src/main/java/ua/com/findcoach/domain/PadawanStatus.java
+++ server-app/src/main/java/ua/com/findcoach/domain/PadawanStatus.java
@@ -3,6 +3,6 @@ package ua.com.findcoach.domain;
 /**
  * Created by pc on 12.10.2015.
  */
-public enum Status {
+public enum PadawanStatus {
         LOOKING_FOR_COACH, TRAINING, NOT_ACTIVE
 }
