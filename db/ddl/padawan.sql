8206f6c94e2881bcc0cf1c7c8f669eb36e242f29
Daemon <akrasilnikovwork@gmail.com>
2015-10-13 21:53:13 +0300

    Create Padawan Repository and update padawan.sql

--- db/ddl/padawan.sql
+++ db/ddl/padawan.sql
@@ -1,7 +1,7 @@
-﻿CREATE TABLE padawans
+﻿CREATE TABLE padawan
 (
   user_id integer NOT NULL,
-  user_status character varying(15) NOT NULL,
+  user_status character varying(20) NOT NULL,
   CONSTRAINT user_id_fk FOREIGN KEY (user_id)
       REFERENCES "user" (user_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
