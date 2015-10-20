5a2eaefa8409a5de728f6e51f0e854d61501ec51
boykas <boykas@live.ru>
2015-10-20 20:13:10 +0300

    mapping new coach's columns

--- db/ddl/coach.sql
+++ db/ddl/coach.sql
@@ -2,6 +2,9 @@ CREATE TABLE coach
 (
   user_id integer NOT NULL,
   status character varying(20) NOT NULL,
+  alias character varying(60),
+   header CHARACTER VARYING(60),
+   coach_describe CHARACTER VARYING(140),
   CONSTRAINT user_id_fk FOREIGN KEY (user_id)
       REFERENCES "user" (user_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
