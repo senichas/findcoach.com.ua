f256b3e16f2b077cfc43ad22207be935b3eacec7
senichas <senichas@gmail.com>
2015-10-22 21:36:07 +0300

    Introduce WD Calendar

--- server-app/src/main/java/ua/com/findcoach/controllers/CoachPublicViewController.java
+++ server-app/src/main/java/ua/com/findcoach/controllers/CoachPublicViewController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
@@ -20,5 +21,10 @@ public class CoachPublicViewController {
         parameters.put("coachFirstName", "Винс");
         parameters.put("coachLastName", "Ломбарди");
         return new ModelAndView("coachPublicCalendar", parameters);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/calendar")
+    public @ResponseBody String fetchEventsForCoach(@PathVariable("coachUserName") String coachUserName) {
+        return "";
     }
 }
