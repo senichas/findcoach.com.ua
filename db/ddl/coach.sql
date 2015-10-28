ee5a91cf5b4ac0ce2ca111e763fcac57dbe26852
boykas <boykas@live.ru>
2015-10-28 15:47:43 +0200

    add new column to coachDb via liquiBase

--- db/ddl/coach.sql
+++ db/ddl/coach.sql
@@ -3,8 +3,8 @@ CREATE TABLE coach
   user_id integer NOT NULL,
   status character varying(20) NOT NULL,
   alias character varying(60),
-   header CHARACTER VARYING(60),
-   coach_describe CHARACTER VARYING(140),
+   title CHARACTER VARYING(60),
+   description CHARACTER VARYING(140),
   CONSTRAINT user_id_fk FOREIGN KEY (user_id)
       REFERENCES "user" (user_id) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
