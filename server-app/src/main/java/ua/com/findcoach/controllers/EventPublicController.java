c62a1bdf47338b45e3a6db7bcdb213455066744b
Andriy Senko <andriy_senko@epam.com>
2015-11-02 17:01:44 +0200

    Add converting Events to Calendar event and add it in rest controller

--- server-app/src/main/java/ua/com/findcoach/controllers/EventPublicController.java
+++ server-app/src/main/java/ua/com/findcoach/controllers/EventPublicController.java
@@ -1,14 +1,17 @@
 package ua.com.findcoach.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import ua.com.findcoach.domain.Event;
-import ua.com.findcoach.services.EventService;
 
-import java.util.List;
+import ua.com.findcoach.api.CalendarEvent;
+import ua.com.findcoach.domain.Event;
+import ua.com.findcoach.services.ConverterService;
+import ua.com.findcoach.services.EventService;
 
 @RequestMapping("/user/{userName}")
 @RestController
@@ -17,9 +20,13 @@ public class EventPublicController {
     @Autowired
     private EventService eventService;
 
+    @Autowired
+    private ConverterService converterService;
+
     @RequestMapping(method = RequestMethod.GET, value = "/events")
-    public List<Event> findEventsForUser(@PathVariable("userName") String userName) {
+    public List<CalendarEvent> findEventsForUser(@PathVariable("userName") String userName) {
         List<Event> events = eventService.findEventsForUserByUserName(userName);
-        return events;
+        List<CalendarEvent> calendarEvents = converterService.convertEventToCalendarEvent(events);
+        return calendarEvents;
     }
 }
