fb76a351e0bf572003c0b364d70c33a583d53662
Andriy Senko <andriy_senko@epam.com>
2015-11-04 18:24:52 +0200

    Change tables structures. Change entities

--- server-app/src/main/java/ua/com/findcoach/domain/Coach.java
+++ server-app/src/main/java/ua/com/findcoach/domain/Coach.java
@@ -1,15 +1,83 @@
 package ua.com.findcoach.domain;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "coach")
-@PrimaryKeyJoinColumn(name = "user_id", referencedColumnName = "user_id")
-public class Coach extends User {
+public class Coach implements User {
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column
+    private boolean isActive;
+
+    @Column
+    private String alias;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private CoachStatus status;
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean isActive() {
+        return isActive;
+    }
+
+    @Override
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    @Override
+    public String getAlias() {
+        return alias;
+    }
+
+    @Override
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
 
     public CoachStatus getStatus() {
         return status;
--- server-app/src/main/java/ua/com/findcoach/domain/Padawan.java
+++ server-app/src/main/java/ua/com/findcoach/domain/Padawan.java
@@ -1,15 +1,83 @@
 package ua.com.findcoach.domain;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "padawan")
-@PrimaryKeyJoinColumn(name = "user_id", referencedColumnName = "user_id")
-public class Padawan extends User{
+public class Padawan implements User {
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column
+    private boolean isActive;
+
+    @Column
+    private String alias;
 
     @Enumerated(EnumType.STRING)
-    @Column (name = "user_status", nullable = false)
+    @Column(name = "user_status", nullable = false)
     private PadawanStatus status;
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean isActive() {
+        return isActive;
+    }
+
+    @Override
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    @Override
+    public String getAlias() {
+        return alias;
+    }
+
+    @Override
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
 
     public PadawanStatus getStatus() {
         return status;
