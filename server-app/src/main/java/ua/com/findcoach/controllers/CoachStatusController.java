65d5b5cc42e3e2e689e8aa59758af2ce8cca530a
boykas <boykas@live.ru>
2015-11-09 12:51:17 +0200

    restful service: get list of statuses. with angular

---
+++ server-app/src/main/java/ua/com/findcoach/controllers/CoachStatusController.java
@@ -0,0 +1,37 @@
+package ua.com.findcoach.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ua.com.findcoach.i18n.LocalizedMessageResolver;
+import ua.com.findcoach.utils.CoachStatusHolder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by DENIS on 07.11.2015.
+ */
+@Controller
+@RequestMapping("/coach/profile")
+public class CoachStatusController {
+    @Autowired
+    private LocalizedMessageResolver messageResolver;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/statuses")
+    @ResponseBody
+    public Map<Enum, String> getCoachStatuses() {
+        Map<Enum, String> statusMap = new HashMap<>();
+        Map<Enum, String> statuses = CoachStatusHolder.getStatusMap();
+
+        statuses
+                .entrySet()
+                .stream()
+                .forEach(enumStringEntry ->
+                                statusMap.put(enumStringEntry.getKey(), messageResolver.getMessage(enumStringEntry.getValue()))
+                );
+        return statusMap;
+    }
+}
---
+++ server-app/src/main/webapp/js/angularJS/app.js
@@ -0,0 +1,4 @@
+var app = angular.module("status", [
+    "ngResource",
+    "statusesControllers"
+]);
