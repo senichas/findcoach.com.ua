4b4396ecd29cbf6c39a5e016b74074022bc89498
senichas <senichas@gmail.com>
2015-11-25 08:14:35 +0200

    Add utility to recreate DB intead drop create it manually

--- db-maintenance/re-create-db.bat
+++ db-maintenance/re-create-db.bat
@@ -1,3 +1,2 @@
-cd "d:\Program Files\PostgreSQL\9.2\bin\" 
-dropdb --host=localhost --username=postgres --no-password findcoach
-createdb --host=localhost --username=postgres --no-password findcoach
+"d:\Program Files\PostgreSQL\9.2\bin\dropdb" --host=localhost --username=postgres --no-password findcoach
+"d:\Program Files\PostgreSQL\9.2\bin\createdb" --host=localhost --username=postgres --no-password findcoach
