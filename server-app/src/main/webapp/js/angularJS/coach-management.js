64e9beeaf0adeaf43ca704c807b6ed332b80752a
senichas <senichas@gmail.com>
2015-12-09 08:16:43 +0200

    Move add padawan controller to coach management JS application

--- server-app/src/main/webapp/js/angularJS/coach-management.js
+++ server-app/src/main/webapp/js/angularJS/coach-management.js
@@ -73,3 +73,55 @@ var saveCycleControllerHandler = function ($scope, DataService, $http) {
 };
 
 coachManagementApplication.controller("cycleController", ["$scope", "DataService", "$http", saveCycleControllerHandler]);
+
+
+coachManagementApplication.factory("AddPadawanDataService", function () {
+
+    return {
+        loggedCoachAlias: loggedCoachAlias
+    };
+});
+
+var addPadawanControllerHandler = function ($scope, AddPadawanDataService, $http) {
+    $scope.endPoint = "/findcoach/coach/" + AddPadawanDataService.loggedCoachAlias + "/padawan-management/basic-info"
+    $scope.padawanData = {};
+    $scope.padawanData.name = "Pertro Vasechkin";
+    $scope.padawanData.email = "padawan@test.com";
+    $scope.padawanData.gender = "MALE";
+    $scope.padawanData.year = "1982";
+
+    $scope.padawanMeasurement = {};
+    $scope.padawanMeasurement.height = "178";
+    $scope.padawanMeasurement.weight = "95";
+    $scope.padawanMeasurement.fatPercentage = "35";
+
+    $scope.padawanProgram = {};
+    $scope.padawanProgram.name = "3 мес сжигание";
+    $scope.padawanProgram.goal = "FAT_BURN";
+    $scope.padawanProgram.notes = "УУууууу";
+    $scope.padawanProgram.startDate = new Date();
+    var endDate = new Date();
+    $scope.padawanProgram.endDate = new Date(endDate.setMonth(endDate.getMonth() + 3));
+
+
+    $scope.submitStep1 = function () {
+
+        var addPadawanData = {};
+        addPadawanData.padawanData = $scope.padawanData;
+        addPadawanData.padawanMeasurement = $scope.padawanMeasurement;
+        addPadawanData.padawanProgram = $scope.padawanProgram;
+
+        $http({
+            method: 'POST',
+            url: $scope.endPoint,
+            data: addPadawanData
+
+        }).then(function successCallback(response) {
+            alert("success");
+        }, function errorCallback(response) {
+            alert("error");
+        });
+    };
+};
+
+coachManagementApplication.controller("addPadawanController", ["$scope", "AddPadawanDataService", "$http", addPadawanControllerHandler]);
