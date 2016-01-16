7b8a31a5f0b68a2e11f171f71b284b7f84647c5b
senichas <senichas@gmail.com>
2016-01-16 17:21:04 +0200

    Add service for find event recurrences

--- server-app/src/main/java/ua/com/findcoach/services/EventService.java
+++ server-app/src/main/java/ua/com/findcoach/services/EventService.java
@@ -2,8 +2,14 @@ package ua.com.findcoach.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ua.com.findcoach.domain.Coach;
 import ua.com.findcoach.domain.Event;
+import ua.com.findcoach.domain.EventRecurrence;
 import ua.com.findcoach.repository.EventRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EventService {
@@ -14,6 +20,12 @@ public class EventService {
     public Event save(Event event) {
         eventRepository.save(event);
         return event;
+    }
+
+    public List<EventRecurrence> findEventRecurrences(Coach coach, LocalDateTime startDateTime, LocalDateTime endDateTime) {
+        List<EventRecurrence> recurrences = new ArrayList<>();
+
+        return recurrences;
     }
 
 //    public List<Event> findEventsForUserByUserName(String userName) {
---
+++ server-app/src/test/java/ua/com/findcoach/services/EventServiceTest.java
@@ -0,0 +1,6 @@
+package ua.com.findcoach.services;
+
+
+public class EventServiceTest {
+
+}
