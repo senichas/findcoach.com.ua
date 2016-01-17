a0f6741c84e2aae85d451ec969374563c23efa47
senichas <senichas@gmail.com>
2016-01-17 14:36:10 +0200

    add database configuration test

--- server-app/src/test/java/ua/com/findcoach/services/EventServiceTest.java
+++ server-app/src/test/java/ua/com/findcoach/services/EventServiceTest.java
@@ -1,6 +1,13 @@
 package ua.com.findcoach.services;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import ua.com.findcoach.conf.DatabaseConfigurarion;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = DatabaseConfigurarion.class)
 public class EventServiceTest {
 
 }
