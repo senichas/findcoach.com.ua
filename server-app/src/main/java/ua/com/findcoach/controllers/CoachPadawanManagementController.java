e4d7ad12c6356ca20ba696ce31e8b6da59eefb7b
senichas <senichas@gmail.com>
2016-07-02 12:38:14 +0300

    Remove old padawan functionality

--- server-app/src/main/java/ua/com/findcoach/controllers/CoachPadawanManagementController.java
+++ server-app/src/main/java/ua/com/findcoach/controllers/CoachPadawanManagementController.java
@@ -2,10 +2,11 @@ package ua.com.findcoach.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ua.com.findcoach.api.AddPadawanBasicInfo;
-import ua.com.findcoach.api.EditPadawanInfoRequest;
 import ua.com.findcoach.domain.Coach;
 import ua.com.findcoach.domain.Measure;
 import ua.com.findcoach.domain.Padawan;
@@ -16,9 +17,6 @@ import ua.com.findcoach.services.ProgramService;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/coach/{userAlias}/padawan-management")
@@ -32,14 +30,6 @@ public class CoachPadawanManagementController {
 
     @Autowired
     private ProgramService programService;
-
-    @RequestMapping(value = "/add-padawan.html", method = RequestMethod.GET)
-    public ModelAndView addPadawanForm() {
-        Map<String, Object> paramerters = new HashMap<>();
-        paramerters.put("coachAlias", coachService.retrieveCurrentCoach().getAlias());
-
-        return new ModelAndView("padawan-management/add-padawan", paramerters);
-    }
 
     @RequestMapping(value = "/basic-info", method = RequestMethod.POST)
     @ResponseBody
@@ -78,32 +68,6 @@ public class CoachPadawanManagementController {
 
         Program p1 = programService.saveProgram(program);
 
-        return "";
-    }
-
-    @RequestMapping(value = "/{padawanId}/edit-padawan.html", method = RequestMethod.GET)
-    public ModelAndView editPadawanPage(@PathVariable Integer padawanId) {
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("coachAlias", coachService.retrieveCurrentCoach().getAlias());
-        Padawan padawan = padawanService.findById(padawanId);
-
-        parameters.put("padawan", padawan);
-        parameters.put("formatter", DateTimeFormatter.ofPattern("YYYY-MM-dd"));
-        return new ModelAndView("padawan-management/edit-padawan", parameters);
-    }
-
-    @RequestMapping(value = "/{padawanId}/edit-padawan.html", method = RequestMethod.POST)
-    @ResponseBody
-    public String updatePadawan(@PathVariable Integer padawanId, @RequestBody EditPadawanInfoRequest editPadawanInfoRequest) {
-        Padawan padawan = padawanService.findById(padawanId);
-
-        padawan.setFirstName(editPadawanInfoRequest.getFirstName());
-        padawan.setLastName(editPadawanInfoRequest.getLastName());
-        padawan.setEmail(editPadawanInfoRequest.getEmail());
-        padawan.setGender(editPadawanInfoRequest.getGender());
-        padawan.setBirthday(editPadawanInfoRequest.getBirthday());
-        padawan.setActive(editPadawanInfoRequest.isActive());
-        Padawan savedPadawan = padawanService.saveAndFlush(padawan);
         return "";
     }
 
