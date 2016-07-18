ee7865d7226f8bc95902e9fe4502383aa7cba3fa
senichas <senichas@gmail.com>
2016-07-18 22:58:13 +0300

    Refactoring deserializer - make possiblity to reuse pattern

--- server-app/src/main/java/ua/com/findcoach/api/deserializers/DateDeserializer.java
+++ server-app/src/main/java/ua/com/findcoach/api/deserializers/DateDeserializer.java
@@ -10,8 +10,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateDeserializer extends JsonDeserializer<LocalDate> {
+    public static final String DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
 
     @Override
     public LocalDate deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
